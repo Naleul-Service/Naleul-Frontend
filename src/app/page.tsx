a6253b4a4bd5cbfe591b5f20185e0f4a0875b166
유림 <yulimh001@gmail.com>
2026-04-24 15:58:49 +0900

    chore: 홈에 button 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 'use client'
 
 import { ChartSection } from '@/src/features/charts/ui/ChartSection'
-import { Button } from '@/src/components/common/Button'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
   return (
-    <main className="pl-[20px]">
-      <Button
-        onClick={() => {
-          router.push('/category')
-        }}
-        variant={'outline'}
-      >
-        카테고리 설정
-      </Button>
+    <main>
       <ChartSection />
     </main>
   )
